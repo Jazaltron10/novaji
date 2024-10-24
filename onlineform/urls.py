2efa0071f709b37d8665b34b355fa610530a5b91
Jazaltron <jazaltron10@gmail.com>
2024-10-24 12:08:41 +0100

    fourth commit

--- onlineform/urls.py
+++ onlineform/urls.py
@@ -3,13 +3,14 @@ from rest_framework.routers import DefaultRouter
 from .views import UserViewSet
 from onlineform import views
 from django.contrib.auth import views as auth_views
-
+from .views import register
 
 
 router = DefaultRouter()
 router.register('users', views.UserViewSet, basename='user')
 urlpatterns = [
     # path('', include(router.urls)),
-    path('login', auth_views.LoginView.as_view(), {'template_name': 'onlineform/login.html'}, name='login'),   
-    path('logout', auth_views.LogoutView.as_view(), name='logout')    
+    path('register/', register, name='register'),
+    path('login/', auth_views.LoginView.as_view(), {'template_name': 'onlineform/login.html'}, name='login'),   
+    path('logout/', auth_views.LogoutView.as_view(), name='logout')    
 ]
--- onlineform/views.py
+++ onlineform/views.py
@@ -1,14 +1,27 @@
 # from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect, render
 from .serializers import UserSerializer
 from rest_framework import viewsets
 from rest_framework.response import Response
 from .models import User
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from .forms import UserForm
 
 
-def Register():
-    pass
+
+def register(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            User = form.save()
+            login(request, User)
+            return redirect("login/")
+    else:
+        form = UserForm()
+    return render(request, "onlineform/register.html", {"form":form})
+            
+        
 
 @login_required
 class UserViewSet (viewsets.ModelViewSet):
