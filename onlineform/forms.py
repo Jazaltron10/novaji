6e1f478c3a17952023d240b6a6e4dfa854a30a11
Jazaltron <jazaltron10@gmail.com>
2024-10-25 08:40:38 +0100

    fifth commit

--- onlineform/forms.py
+++ onlineform/forms.py
@@ -1,7 +1,8 @@
-from django.forms import ModelForm 
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from onlineform.models import User
 
-class UserForm(ModelForm):
+class UserForm(UserCreationForm):  # Inherit from UserCreationForm for password hashing
     class Meta:
-        model: User
-        fields = ['first_name','last_name','phone_number','email','password','date_of_birth']
+        model = User
+        fields = ['first_name', 'last_name', 'phone_number', 'email', 'date_of_birth']
--- onlineform/views.py
+++ onlineform/views.py
@@ -1,30 +1,25 @@
-# from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, redirect, render
-from .serializers import UserSerializer
-from rest_framework import viewsets
-from rest_framework.response import Response
-from .models import User
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
 from django.contrib.auth import login
+from .serializers import UserSerializer
+from .models import User
+from rest_framework import viewsets
 from .forms import UserForm
-
-
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == "POST":
         form = UserForm(request.POST)
         if form.is_valid():
-            User = form.save()
-            login(request, User)
-            return redirect("login/")
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)
+            return redirect("user-list")
     else:
         form = UserForm()
-    return render(request, "onlineform/register.html", {"form":form})
-            
-        
+    return render(request, "onlineform/registration.html", {"form": form})
 
-@login_required
-class UserViewSet (viewsets.ModelViewSet):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    
+    # permission_classes = [IsAuthenticated]  # Make sure only authenticated users can access this
